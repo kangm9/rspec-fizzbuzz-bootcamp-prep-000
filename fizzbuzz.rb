ce77f42bf267bf0a289015e6195b4ef8ba8b6bc0
Min Kang <kangm914@gmail.com>
2019-05-11 21:14:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -9,8 +9,19 @@ def fizzbuzz(number)
      else if number % 5 == 0
         "Buzz"
       else 
-          "nil"
+          "nil
      end
     end
   end
 end
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0 
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else 
+    nil
+  end
+end 
